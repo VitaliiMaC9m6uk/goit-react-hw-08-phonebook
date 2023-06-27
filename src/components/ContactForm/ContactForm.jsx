765c7cd5c684b5b4498ddd04b5068896e9160e46
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-06-27 23:28:08 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -40,8 +40,7 @@ export const ContactForm = () => {
         <input
           className={css.formName}
           type="text"
-          name="name"
-          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          name="name"         
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           placeholder="Enter name"
@@ -54,8 +53,7 @@ export const ContactForm = () => {
         <input
           className={css.formNumber}
           type="tel"
-          name="number"
-          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          name="number"          
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           placeholder="Enter phone number"
